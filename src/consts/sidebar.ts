c1d048c568c397c223e5daacd18dc92ba91966a1
Leap-Chanvuthy <chanvuthy.leap@student.cadt.edu.kh>
2026-01-16 23:06:59 +0700

    lock sidebar where feature are not implemted yet

--- src/consts/sidebar.ts
+++ src/consts/sidebar.ts
@@ -9,6 +9,12 @@ import {
   Warehouse,
   HelpCircle,
   Truck,
+  Factory,
+  PackageSearch,
+  PencilRuler,
+  Users,
+  CircleDollarSign,
+  NotepadText,
 } from "lucide-react";
 import { ROLES, Role } from "./role";
 
@@ -17,6 +23,7 @@ export interface SidebarItem {
   url: string;
   icon: any;
   roles?: Role[];
+  isLocked?: boolean | false;
 }
 
 export interface SidebarGroup {
@@ -52,37 +59,95 @@ export const SIDEBAR_CONFIG: SidebarGroup[] = [
         title: "Supplier",
         url: "/supplier",
         icon: Truck,
-        roles: [ROLES.ADMIN],
+        roles: [ROLES.ADMIN , ROLES.STOCK_CONTROLLER],
+      },
+      {
+        title: "Raw Materials",
+        url: "/raw-materials",
+        icon: Factory,
+        roles: [ROLES.ADMIN , ROLES.STOCK_CONTROLLER],
+        isLocked: true,
+      },
+      {
+        title: "Production",
+        url: "/production",
+        icon: PackageSearch,
+        roles: [ROLES.ADMIN , ROLES.STOCK_CONTROLLER],
+        isLocked: true,
       },
     ],
   },
-
   {
     label: "Catalogs",
     items: [
       {
-        title: "Warehouses",
+        title: "Muti Warehouses",
         url: "/warehouses",
         icon: Warehouse,
-        roles: [ROLES.ADMIN],
+        roles: [ROLES.ADMIN , ROLES.STOCK_CONTROLLER],
       },
       {
         title: "Categories",
         url: "/categories",
         icon: Boxes,
-        roles: [ROLES.ADMIN],
+        roles: [ROLES.ADMIN , ROLES.STOCK_CONTROLLER],
+      },
+      {
+        title: "Unit of Measurement",
+        url: "/unit-of-measurement",
+        icon: PencilRuler,
+        roles: [ROLES.ADMIN , ROLES.STOCK_CONTROLLER],
+        isLocked: true,
+      },
+    ],
+  },
+  {
+    label: "Sale Management & POS",
+    items: [
+      {
+        title: "Customer",
+        url: "/customer",
+        icon: Users,
+        roles: [ROLES.ADMIN , ROLES.VENDER],
+        isLocked: true,
+      },
+      {
+        title: "Sale Orders",
+        url: "/sale-orders",
+        icon: CircleDollarSign,
+        roles: [ROLES.ADMIN , ROLES.VENDER],
+        isLocked: true,
+      },
+    ],
+  },
+    {
+    label: "Report & Analytics",
+    items: [
+      {
+        title: "Financial Report",
+        url: "/financial-report",
+        icon: NotepadText,
+        roles: [ROLES.ADMIN , ROLES.VENDER],
+        isLocked: true,
       },
     ],
   },
   {
     label: "Settings",
     items: [
-      { title: "Settings", url: "/settings", icon: Settings },
+      { 
+        title: "Settings", 
+        url: "/settings", 
+        icon: Settings , 
+        roles: [ROLES.ADMIN] , 
+        isLocked: true,
+      },
       {
         title: "Company",
         url: "/company",
         icon: User,
         roles: [ROLES.ADMIN],
+        isLocked: true,
       },
     ],
   },
