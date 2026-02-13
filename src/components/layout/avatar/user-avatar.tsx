7eb7c69215ce74204be748fd60e2080a22d587ed
Leap-Chanvuthy <chanvuthy.leap@student.cadt.edu.kh>
2026-02-13 14:10:48 +0700

    .

--- src/components/layout/avatar/user-avatar.tsx
+++ src/components/layout/avatar/user-avatar.tsx
@@ -1,18 +1,21 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useAuth } from "@/hooks/useAuth";
+import { Link } from "react-router-dom";
 
 const UserAvatar = () => {
 
     const {user} = useAuth(); 
 
   return (
-    <Avatar>
-      <AvatarImage
-        src={user?.profile_picture || "https://leapchanvuthy.vercel.app/images/Leapchanvuthy.png"}
-        alt="User avatar"
-      />
-      <AvatarFallback>{user?.name?.[0] || "U"}</AvatarFallback>
-    </Avatar>
+    <Link to="/profile?tab=profile">
+      <Avatar>
+        <AvatarImage
+          src={user?.profile_picture || "https://leapchanvuthy.vercel.app/images/Leapchanvuthy.png"}
+          alt="User avatar"
+        />
+        <AvatarFallback>{user?.name?.[0] || "U"}</AvatarFallback>
+      </Avatar>
+    </Link>
   );
 };
 
